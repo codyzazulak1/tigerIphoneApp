6bdc57f23bb7f4964ec019c96e284ed1c689c42a
codyzazulak1 <codyzazulak1@gmail.com>
2014-10-21 21:00:13 -0700

    Initial Commit

---
+++ LionsAndTigers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LionsAndTigers
+//
+//  Created by Cody Zazulak on 2014-10-21.
+//  Copyright (c) 2014 Cody Zazulak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
